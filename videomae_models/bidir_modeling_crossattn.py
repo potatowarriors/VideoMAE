adda154fc14bd200d7d8db674c921655c3292954
potatowarriors <kide004@gmail.com>
2023-02-15 23:18:10 +0900

    change sequence cross, mhsa

--- videomae_models/bidir_modeling_crossattn.py
+++ videomae_models/bidir_modeling_crossattn.py
@@ -281,21 +281,9 @@ class Block(nn.Module):
                  act_layer=nn.GELU, norm_layer=nn.LayerNorm,attn_head_dim=None):
         super().__init__()
         self.cross = None
-        self.norm1 = norm_layer(dim)
-        self.attn = Attention(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            attn_drop=attn_drop, proj_drop=drop, attn_head_dim=attn_head_dim)
-        
-        self.clip_ln_1 = norm_layer(dim)
-        self.clip_attn = nn.MultiheadAttention(dim, num_heads)
         
         if num_layer in reduce_position:
             self.cross = True
-            if num_layer == reduce_position[0]:
-                print('activate sinusoidal position embed for clip!')
-                self.spatial_posembed = get_sinusoid_encoding_table(1568, dim)
-            else:
-                self.spatial_posembed = None
             self.ln_s2t = norm_layer(dim) # 이건 cross attn 전용 layer norm으로 변경해야 한다.
             self.s2t_cross = CrossAttentionS2T(
                dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
@@ -303,6 +291,14 @@ class Block(nn.Module):
             self.ln_t2s = norm_layer(dim)
             self.t2s_cross = CrossAttentionT2S(dim, num_heads)
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=drop, attn_head_dim=attn_head_dim)
+        
+        self.clip_ln_1 = norm_layer(dim)
+        self.clip_attn = nn.MultiheadAttention(dim, num_heads)
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         
         # Time path
@@ -330,21 +326,14 @@ class Block(nn.Module):
 
     def forward(self,s_x, t_x):
         if self.gamma_1 is None:
-            B = t_x.shape[0]
-            s_x = s_x + self.drop_path((self.clip_attention(self.clip_ln_1(s_x)))) # CLIP space attention
-            t_x = t_x + self.drop_path(self.attn(self.norm1(t_x))) # VMAE space-time joint attention
-            
-            #s_x = rearrange(s_x, '(b t) n d -> b t n d', t=16) # cross attention을 위해 shape을 수정해준다. center frame만 쓰니까 잠시 꺼둔다.
-            
             if self.cross is not None:
-                if self.spatial_posembed is not None:
-                    s_x = rearrange(s_x, '(b t) n d -> b (t n) d', b=B)
-                    s_x = s_x + self.spatial_posembed.expand(B, -1, -1).type_as(s_x).to(s_x.device).clone().detach()
-                    s_x = rearrange(s_x, 'b (t n) d -> (b t) n d', t=8)
                 injected_s_x = s_x + self.drop_path(self.t2s_cross(self.ln_t2s(s_x), t_x)) # temporal to spatial cross attn
                 injected_t_x = t_x + self.drop_path(self.s2t_cross(s_x, self.ln_s2t(t_x))) # Cross attention space to time
                 s_x = injected_s_x
                 t_x = injected_t_x
+                
+            s_x = s_x + self.drop_path((self.clip_attention(self.clip_ln_1(s_x)))) # CLIP space attention
+            t_x = t_x + self.drop_path(self.attn(self.norm1(t_x))) # VMAE space-time joint attention
             
             s_x = s_x + self.drop_path(self.clip_mlp(self.clip_ln_2(s_x))) # pass CLIP FFN
             t_x = t_x + self.drop_path(self.mlp(self.norm2(t_x))) # pass VMAE FFN
